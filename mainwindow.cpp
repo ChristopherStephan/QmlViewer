ef210cb53b7805fe736a217da3ef8205d451f66e
Christopher Stephan <christopher.stephan@miele.com>
2023-11-18 17:18:52 +0100

    Add click event to fist button to show QML file in a new window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QQuickWidget>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +12,13 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QQuickWidget *view = new QQuickWidget;
+    view->setSource(QUrl::fromLocalFile("/Users/destec2/Documents/QmlViewer/QmlViewer/imports/blueRectangle.qml"));
+    view->show();
 }
 

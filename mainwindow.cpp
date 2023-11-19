493125267bfbba071732a5aa14c9acf12a5a530f
Christopher Stephan <christopher.stephan@miele.com>
2023-11-19 08:28:41 +0100

    Pass the path of the qml as a property in order to open the correct qml file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -15,14 +15,17 @@ MainWindow::MainWindow(QWidget *parent)
 
     std::string path = "/Users/destec2/Documents/QmlViewer/QmlViewer/imports/";
 
-    for (const auto & file : fs::directory_iterator(path)){
+    for (const auto & file : fs::directory_iterator(path)){ 
         QString filename = QString::fromStdString(file.path().filename().string());
         QPushButton *button = new QPushButton(filename, this);
-        ui->horizontalLayout->addWidget(button);
 
-        // Connect button click to slot for changing view
+        QString path = QString::fromStdString(file.path().string());
+        button->setProperty("path", QVariant(path));
+
         connect(button, &QPushButton::clicked, this, &MainWindow::on_pushButton_clicked);
+
         std::cout << file.path() << std::endl;
+        ui->horizontalLayout->addWidget(button);
     }
 }
 
@@ -31,11 +34,14 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
 void MainWindow::on_pushButton_clicked()
 {
-    QQuickWidget *view = new QQuickWidget;
-    view->setSource(QUrl::fromLocalFile("/Users/destec2/Documents/QmlViewer/QmlViewer/imports/blueRectangle.qml"));
-    setCentralWidget(view);
+    QPushButton* button = qobject_cast<QPushButton*>(sender());
+    if(button) {
+        QString path = button->property("path").toString();
+        QQuickWidget *view = new QQuickWidget;
+        view->setSource(QUrl::fromLocalFile(path));
+        setCentralWidget(view);
+    }
 }
 
